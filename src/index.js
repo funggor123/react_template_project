a224e8f9fe893e5c8691c00df54c411d6621d77c
awdaw <Awdaw>
2019-10-07 01:08:11 +0800

    Sefse

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<DatePicker />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
