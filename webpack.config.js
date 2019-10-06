a851f6c1d1315dd128545159ef8556c87aa7f450
awdaw <Awdaw>
2019-10-07 00:50:45 +0800

    serfse

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+var path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: {
+        index: './src/index.js'
+
+    },
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'js/[name]-chain-app.js',
+
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+						"presets": ["@babel/preset-env", "@babel/preset-react"]
+                    }
+
+                }
+
+            }, {
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			  }
+
+        ]
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+		template: "./public/index.html",
+		filename: "./index.html"
+	  })
+	]
+}
